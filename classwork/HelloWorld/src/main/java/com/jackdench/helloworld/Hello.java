cebc9e906612f4ad4301b7bfaaa631acc9fab8d9
Jack Dench <jdench1989@gmail.com>
2024-08-28 13:33:50 +0100

    Hello world in netbeans

---
+++ classwork/HelloWorld/src/main/java/com/jackdench/helloworld/Hello.java
@@ -0,0 +1,13 @@
+package com.jackdench.helloworld;
+
+/**
+ * @author jackdench
+ * email: jdench1989@gmail.com
+ * date: 28/08/2024
+ * purpose: My first Hello World program in an IDE
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jack!");
+    }
+}
